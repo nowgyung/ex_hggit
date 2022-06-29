6d882264696aa53488df4145a07751bc3672d463
nowgyung <vetibeti@naver.com>
2022-06-29 14:24:43 +0900

    220629_java

---
+++ java_work/ex_java/src/ex_java/AA.java
@@ -0,0 +1,23 @@
+package ex_java;
+
+public class AA {
+	
+	public static void main(String[] args) {
+		
+		/*
+		System.out.println(7);
+		System.out.println(3.15);
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + " 는 실수입니다.");
+		*/
+		
+		System.out.println("2 + 5 = " + 2 + 5);
+		System.out.println("2 + 5 = " + (2 + 5));
+		
+		
+		
+		
+		
+	}
+
+}
