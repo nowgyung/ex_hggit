bac7e13baa7303f45de55e55cde6544aaefaee76
nowgyung <vetibeti@naver.com>
2022-07-24 01:58:20 +0900

    220723

---
+++ java_work/study_220723/src/study_220723/Ex01.java
@@ -0,0 +1,16 @@
+package study_220723;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("age: "+ 17);
+		String str = "age: "+ 17;
+		System.out.println(str);
+		
+		
+		double e = 2.718281;
+		String se = String.valueOf(e);
+		System.out.println(se);
+		System.out.println(se.getClass().getName());
+	}
+
+}
