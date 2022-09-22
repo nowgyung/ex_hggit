cc215bf3ff6fa4ad3ae42ec9f861dbb56a724140
nowgyung <vetibeti@naver.com>
2022-09-22 23:15:30 +0900

    220922

---
+++ intellij_work/test/src/main/java/ex0922/PrintableInterface.java
@@ -0,0 +1,17 @@
+package ex0922;
+
+interface  Printable{
+    public void print(String doc);
+}
+class Printer implements Printable{ // Printable을 구현하는 Printer 클래스
+    @Override
+    public void print(String doc){ //  오버라이딩 관계 성립
+        System.out.println(doc);
+    }
+}
+public class PrintableInterface {
+    public static void main(String[] args) {
+        Printable prn = new Printer(); // Printable형 참조변수 선언 가능
+        prn.print("Hello Java"); // 추상 메소드 호출할 수 있음
+    }
+}
---
+++ intellij_work/test/src/main/java/ex0922/PrinterDriver.java
@@ -0,0 +1,33 @@
+package ex0922;
+
+interface Printable2{ //MS가 정의하고 제공한 인터페이스
+    public void print(String doc);
+}
+class SPrinterDriver implements Printable{ // S사가 정의한 클래스
+    @Override
+    public void print(String doc){
+        System.out.println("From Samsung printer");
+        System.out.println(doc);
+    }
+}
+class LPrinterDriver implements Printable{ // L사가 정의한 클래스
+    @Override
+    public void print(String doc){
+        System.out.println("From LG Printer");
+        System.out.println(doc);
+    }
+}
+public class PrinterDriver {
+    public static void main(String[] args) {
+        String myDoc = "This is a report about...";
+
+        // 삼성 프린터로 출력
+        Printable prn = new SPrinterDriver();
+        prn.print(myDoc);
+        System.out.println();
+
+        // LG 프린터로 출력
+        prn = new LPrinterDriver();
+        prn.print(myDoc);
+    }
+}
